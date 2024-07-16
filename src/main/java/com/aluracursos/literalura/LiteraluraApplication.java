04c498567e95e85f9187c871e8b588f96dd911e8
Angiii12 <angi.mari.rincon@gmail.com>
2024-07-16 14:40:44 -0300

    main and application

--- src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -1,13 +1,26 @@
 package com.aluracursos.literalura;
 
+import com.aluracursos.literalura.principal.Main;
+import com.aluracursos.literalura.repository.AuthorRepository;
+import com.aluracursos.literalura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Principal;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+	@Autowired
+	private BookRepository bookRepository;
+	@Autowired
+	private AuthorRepository authorRepository;
+	public static void main(String[] args) {SpringApplication.run(LiteraluraApplication.class, args);}
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiteraluraApplication.class, args);
+	@Override
+	public void run(String... args) throws Exception{
+		Main m = new Main(bookRepository, authorRepository);
+		m.menu();
 	}
-
 }
---
+++ src/main/java/com/aluracursos/literalura/principal/Main.java
@@ -0,0 +1,153 @@
+package com.aluracursos.literalura.principal;
+
+import com.aluracursos.literalura.model.Author;
+import com.aluracursos.literalura.model.Book;
+import com.aluracursos.literalura.model.BookData;
+import com.aluracursos.literalura.model.ResultData;
+import com.aluracursos.literalura.repository.AuthorRepository;
+import com.aluracursos.literalura.repository.BookRepository;
+import com.aluracursos.literalura.service.APIConsumption;
+import com.aluracursos.literalura.service.DataConversor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private Scanner keyboard = new Scanner(System.in);
+    private APIConsumption api = new APIConsumption();
+    private final String URL_BASE = "https://gutendex.com/books/";
+    private DataConversor conversor = new DataConversor();
+    private List<Book> books;
+    private List<Book> booksLan;
+    private List<Author> authors;
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+
+    public Main(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    public void menu() {
+        var option = -1;
+        while (option != 0) {
+            var m = """
+                    1 - Search book by name 
+                    2 - List registered books
+                    3 - List registered authors
+                    4 - List authors alive in a given year
+                    5 - List books by language
+                    0 - Salir
+                    """;
+            System.out.println(m);
+            option = keyboard.nextInt();
+            keyboard.nextLine();
+            switch (option) {
+                case 1:
+                    searchBookByName();
+                    break;
+                case 2:
+                    ListRegisteredBooks();
+                    break;
+                case 3:
+                    ListRegisteredAuthors();
+                    break;
+                case 4:
+                    SearchAuthorsAliveByDate();
+                    break;
+                case 5:
+                    ListBooksByLanguage();
+                    break;
+                case 0:
+                    System.out.println("Closing...");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+
+    private void ListBooksByLanguage() {
+        System.out.println("Insert the first 2 characters of the language: ");
+        var lan = keyboard.nextLine();
+        try{
+            booksLan = bookRepository.findByLanguages(lan);
+            if(booksLan.isEmpty()) {
+                System.out.println("No Books Found");
+            }
+            booksLan.forEach(System.out::println);
+        } catch (RuntimeException e){
+            System.out.println("Invalid language");
+        }
+
+    }
+
+    private void SearchAuthorsAliveByDate() {
+        System.out.println("Insert year: ");
+        var in = Integer.parseInt(keyboard.nextLine());
+        try{
+            List<Author> authors = authorRepository.findAuthorsByDeathYear(in);
+            if(authors.isEmpty()) {
+                System.out.println("No authors found");
+            }
+            authors.forEach(System.out::println);
+        }catch (RuntimeException e){
+            System.out.println("Invalid year");
+        }
+    }
+
+    private void ListRegisteredBooks() {
+        books = bookRepository.findAll();
+        if (books.isEmpty()) {
+            System.out.println("No books found");
+        } else {
+            books.forEach(System.out::println);
+        }
+    }
+
+    private void ListRegisteredAuthors() {
+        authors = authorRepository.findAll();
+        if (authors.isEmpty()) {
+            System.out.println("No authors found");
+        } else {
+            authors.forEach(System.out::println);
+        }
+    }
+
+    private BookData getBook() {
+        System.out.println("Insert book name: ");
+        var name = keyboard.nextLine();
+        var json = api.obtainData(URL_BASE + "?search=" + name.replace(" ", "%20").toLowerCase());
+        BookData book = conversor.obtainData(json, BookData.class);
+        return book;
+    }
+    private void searchBookByName() {
+        boolean bookFound = false;
+        do {
+            try {
+                BookData bookData = getBook();
+
+                if (bookData.results().isEmpty()) {
+                    throw new RuntimeException("No se encontraron libros para el nombre dado.");
+                }
+
+                ResultData firstResult = bookData.results().get(0);
+
+                Book book = new Book(firstResult);
+                Author author = new Author(firstResult.authors().get(0));
+
+                System.out.println(book);
+                bookRepository.save(book);
+                authorRepository.save(author);
+
+                bookFound = true;
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Please try again");
+            }
+        } while (!bookFound);
+    }
+}
